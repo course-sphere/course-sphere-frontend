fed57c8909a829340e1f69f7886b8f81c7fca1c9
akagiyuu <khanghuynh0245@gmail.com>
2026-01-26 18:58:47 +0700

    feat(axios): init axios

---
+++ src/lib/api-client/index.ts
@@ -0,0 +1,28 @@
+import axios, {
+    AxiosError,
+    type AxiosResponse,
+    type InternalAxiosRequestConfig,
+} from 'axios';
+import { config } from './config';
+
+export const apiClient = axios.create(config);
+
+apiClient.interceptors.request.use(
+    (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+        console.log(`${config.method?.toUpperCase()} ${config.url}`);
+        return config;
+    },
+    (error: AxiosError): Promise<AxiosError> => {
+        console.error('Request Error:', error);
+        return Promise.reject(error);
+    },
+);
+
+apiClient.interceptors.response.use(
+    (response: AxiosResponse): AxiosResponse => {
+        return response;
+    },
+    (error: AxiosError): Promise<AxiosResponse> => {
+        return Promise.reject(error);
+    },
+);
