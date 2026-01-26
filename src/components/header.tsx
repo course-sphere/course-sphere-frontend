4241e984b9544a848e48d3bbd9abcc93a473852d
akagiyuu <khanghuynh0245@gmail.com>
2026-01-26 22:03:51 +0700

    fix(component/header): remove unneed state

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-import { Sheet, SheetContent, SheetFooter } from '@/components/ui/sheet';
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from '@/components/ui/sheet';
 import { Button, buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useState } from 'react';
@@ -18,80 +26,90 @@ export interface HeaderProps {
     items: { label: string; href: string }[];
 }
 
-export function Header({ items }: HeaderProps) {
-    const [open, setOpen] = useState(false);
-
+function Auth() {
     return (
-        <header
-            className={cn(
-                'sticky top-5 z-50',
-                'mx-auto w-full max-w-3xl rounded-lg border shadow-lg',
-                'bg-background/95 supports-[backdrop-filter]:bg-background/80 backdrop-blur',
-            )}
-        >
-            <NavigationMenu className="relative w-full max-w-full justify-between p-2">
-                <div className="hover:bg-accent flex cursor-pointer rounded-md px-2 py-1 duration-100">
-                    <Icon />
-                </div>
-                <div className="absolute left-1/2 -translate-x-1/2 transform">
-                    <NavigationMenuList className="hidden items-center gap-1 md:flex">
-                        {items.map((item) => (
-                            <NavigationMenuLink
-                                asChild
-                                className={navigationMenuTriggerStyle()}
-                            >
-                                <Link
-                                    className={buttonVariants({
-                                        variant: 'ghost',
-                                        size: 'sm',
-                                    })}
-                                    href={item.href}
+        <>
+            <Button size="sm" asChild>
+                <Link href="/auth/login">Login</Link>
+            </Button>
+            <Button size="sm" variant="secondary" asChild>
+                <Link href="/auth/register">Register</Link>
+            </Button>
+        </>
+    );
+}
+
+export function Header({ items }: HeaderProps) {
+    return (
+        <header className="sticky top-5 z-50 px-10">
+            <div className="bg-background/95 supports-[backdrop-filter]:bg-background/80 mx-auto w-full max-w-4xl rounded-lg border shadow-lg backdrop-blur">
+                <NavigationMenu className="relative w-full max-w-full justify-between p-2">
+                    <div className="hover:bg-accent flex cursor-pointer rounded-md px-2 py-1 duration-100">
+                        <Icon />
+                    </div>
+                    <div className="absolute left-1/2 -translate-x-1/2 transform">
+                        <NavigationMenuList className="hidden items-center gap-1 md:flex">
+                            {items.map((item) => (
+                                <NavigationMenuLink
+                                    key={item.label}
+                                    asChild
+                                    className={navigationMenuTriggerStyle()}
                                 >
-                                    {item.label}
-                                </Link>
-                            </NavigationMenuLink>
-                        ))}
-                    </NavigationMenuList>
-                </div>
-                <div className="flex items-center gap-2">
-                    <Button size="sm">Login</Button>
-                    <Button size="sm" variant="secondary">
-                        Register
-                    </Button>
-                    <Sheet open={open} onOpenChange={setOpen}>
-                        <Button
-                            size="icon"
-                            variant="outline"
-                            onClick={() => setOpen(!open)}
-                            className="md:hidden"
-                        >
-                            <MenuIcon className="size-4" />
-                        </Button>
-                        <SheetContent
-                            className="bg-background/95 supports-[backdrop-filter]:bg-background/80 gap-0 backdrop-blur-lg"
-                            side="left"
-                        >
-                            <div className="grid gap-y-2 overflow-y-auto px-4 pt-12 pb-5">
-                                {items.map((item) => (
                                     <Link
                                         className={buttonVariants({
                                             variant: 'ghost',
-                                            className: 'justify-start',
+                                            size: 'sm',
                                         })}
                                         href={item.href}
                                     >
                                         {item.label}
                                     </Link>
-                                ))}
+                                </NavigationMenuLink>
+                            ))}
+                        </NavigationMenuList>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Sheet>
+                            <div className="hidden space-x-3 md:block">
+                                <Auth />
                             </div>
-                            <SheetFooter>
-                                <Button variant="outline">Sign In</Button>
-                                <Button>Get Started</Button>
-                            </SheetFooter>
-                        </SheetContent>
-                    </Sheet>
-                </div>
-            </NavigationMenu>
+                            <SheetTrigger asChild>
+                                <Button
+                                    size="icon"
+                                    variant="outline"
+                                    className="md:hidden"
+                                >
+                                    <MenuIcon className="size-4" />
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side="left">
+                                <SheetHeader>
+                                    <SheetTitle>
+                                        <Icon />
+                                    </SheetTitle>
+                                </SheetHeader>
+                                <div className="grid gap-y-2 overflow-y-auto px-4 pt-12 pb-5">
+                                    {items.map((item) => (
+                                        <Link
+                                            key={item.label}
+                                            className={buttonVariants({
+                                                variant: 'ghost',
+                                                className: 'justify-start',
+                                            })}
+                                            href={item.href}
+                                        >
+                                            {item.label}
+                                        </Link>
+                                    ))}
+                                </div>
+                                <SheetFooter>
+                                    <Auth />
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </NavigationMenu>
+            </div>
         </header>
     );
 }
