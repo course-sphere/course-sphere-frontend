eecb92125a3ab58814cb57be451c97a23b4f87b1
akagiyuu <khanghuynh0245@gmail.com>
2026-01-28 00:30:17 +0700

    fix(component/header): add spacing between logo and nav list

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -58,7 +58,7 @@ export function Header({ items }: HeaderProps) {
                         isFloating ? 'max-w-full' : 'max-w-6xl',
                     )}
                 >
-                    <div className="flex cursor-pointer rounded-md px-2 py-1 duration-100">
+                    <div className="flex cursor-pointer space-x-4 rounded-md px-2 py-1 duration-100">
                         <Logo className="size-6" />
                         <NavigationMenuList className="hidden items-center gap-1 lg:flex">
                             {items.map((item) => (
